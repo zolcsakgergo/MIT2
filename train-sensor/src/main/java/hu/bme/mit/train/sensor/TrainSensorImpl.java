ad8d77d6e27d3ac39839d737852662027616f1cc
zolcsakgergo <zolcsakgergo2001@gmail.com>
2024-03-18 18:02:14 +0100

    alarm feat. added

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -22,6 +22,10 @@ public class TrainSensorImpl implements TrainSensor {
 
 	@Override
 	public void overrideSpeedLimit(int speedLimit) {
+		if((speedLimit > 500 || speedLimit < 0) || (speedLimit < this.speedLimit*0.5)){
+			user.setAlarmState(true);
+		}
+
 		this.speedLimit = speedLimit;
 		controller.setSpeedLimit(speedLimit);
 	}
